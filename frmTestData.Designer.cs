95259e1c700cc39c17ae392eb6045dc10b4b7f6c
darrylle <darrylles@gmail.com>
2015-07-25 08:16:37 +0100

    DB connection test

---
+++ frmTestData.Designer.cs
@@ -0,0 +1,132 @@
+﻿namespace Query
+{
+    partial class frmTestData
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtEmpLogin = new System.Windows.Forms.TextBox();
+            this.txtFullName = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.cmdLogin = new System.Windows.Forms.Button();
+            this.txtResult = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtEmpLogin
+            // 
+            this.txtEmpLogin.Location = new System.Drawing.Point(100, 15);
+            this.txtEmpLogin.Name = "txtEmpLogin";
+            this.txtEmpLogin.Size = new System.Drawing.Size(100, 20);
+            this.txtEmpLogin.TabIndex = 0;
+            this.txtEmpLogin.Leave += new System.EventHandler(this.txtEmpLogin_Leave);
+            // 
+            // txtFullName
+            // 
+            this.txtFullName.Location = new System.Drawing.Point(99, 41);
+            this.txtFullName.Name = "txtFullName";
+            this.txtFullName.Size = new System.Drawing.Size(100, 20);
+            this.txtFullName.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.label1.Location = new System.Drawing.Point(10, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(79, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Network Login:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.label2.Location = new System.Drawing.Point(31, 44);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(57, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Full Name:";
+            // 
+            // cmdLogin
+            // 
+            this.cmdLogin.Location = new System.Drawing.Point(99, 67);
+            this.cmdLogin.Name = "cmdLogin";
+            this.cmdLogin.Size = new System.Drawing.Size(97, 22);
+            this.cmdLogin.TabIndex = 4;
+            this.cmdLogin.Text = "Login";
+            this.cmdLogin.UseVisualStyleBackColor = true;
+            this.cmdLogin.Click += new System.EventHandler(this.cmdLogin_Click);
+            // 
+            // txtResult
+            // 
+            this.txtResult.Location = new System.Drawing.Point(49, 160);
+            this.txtResult.Name = "txtResult";
+            this.txtResult.Size = new System.Drawing.Size(150, 20);
+            this.txtResult.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.ForeColor = System.Drawing.Color.Red;
+            this.label3.Location = new System.Drawing.Point(12, 144);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(97, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Connection Result:";
+            // 
+            // frmTestData
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(217, 202);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtResult);
+            this.Controls.Add(this.cmdLogin);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtFullName);
+            this.Controls.Add(this.txtEmpLogin);
+            this.Name = "frmTestData";
+            this.Text = "Test Data";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtEmpLogin;
+        private System.Windows.Forms.TextBox txtFullName;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button cmdLogin;
+        private System.Windows.Forms.TextBox txtResult;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ frmTestData.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Query
+{
+    public partial class frmTestData : Form
+    {
+        public frmTestData()
+        {
+            InitializeComponent();
+        }
+
+        private void cmdLogin_Click(object sender, EventArgs e)
+        {
+            
+            //Initialise variables...
+            //
+            string strDataPath =
+                        "\\\\Server_pc\\M\\1 NTFS\\Data\\Dev\\Access" +
+                        "\\BAE\\TIM\\Query\\dev\\dat\\query_be.mdb";
+
+            string strConn =
+                        "Provider=Microsoft.ACE.OLEDB.12.0;" +
+                        "Data Source='" + strDataPath + "';" +
+                        "Persist Security Info=False;";
+
+            string strQuery =
+                        "SELECT pk " +
+                        "FROM   tblEmp " +
+                        "WHERE  nw_login = '" + txtEmpLogin.Text + "' " +
+                        "AND    full_name = '" + txtFullName.Text + "';";
+            
+            //Intialise the connection criteria...
+            //
+            DataConnection.DataConnector dtc = 
+                new DataConnection.DataConnector (strConn);
+
+            //Connect and retrieve the query data...
+            //
+            DataTable dtb = dtc.DataSelect (strQuery);
+
+            //Check and display results...
+            //
+            if( dtb.Rows.Count > 0)
+            {
+                //Show 'pk' value in first record returned [0]...
+                //MessageBox.Show("User pk value = " + dtb.Rows[0]["pk"].ToString()); 
+                txtResult.Text  = "User pk value = " + dtb.Rows[0]["pk"].ToString();
+            }
+            else
+            {
+                txtResult.Text  = "This user does NOT exist!";
+            }
+        }
+
+    }
+}
